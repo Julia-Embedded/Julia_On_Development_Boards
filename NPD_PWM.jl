94305e272154bf544d56ea4707dee4de5cf75fd8
Frank Applin <frankapplin@yahoo.com>
2017-10-16 10:01:22 -0400

    Update NPD_PWM.jl

--- NPD_PWM.jl
+++ NPD_PWM.jl
@@ -1,12 +1,12 @@
 npdProc = addprocs(["julia-user@NODE-NANOPIDUO"],dir="/home/julia-user/julia-0.6.0/bin/")
 
-include("GPIO_NPD.jl")
+include("GPIO_Device.jl")
 
-import GPIO_NPD
+import GPIO_Device
 
-using GPIO_NPD
+using GPIO_Device
 
-npd = GPIO_NPD.NPDGPIO()
+npd = GPIO_Device.DeviceGPIO()
 
 initialize(npd, "NANOPIDUO.xml")
 
